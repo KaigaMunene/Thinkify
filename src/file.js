a6d11a3b0703d9bec3efdfceeaed8cdd1d24f764
KaigaMunene <kaigamunene@gmail.com>
2024-07-06 16:31:40 +0300

    make changes

--- src/file.js
+++ src/file.js
@@ -1,11 +1,8 @@
 // File: example.js
 
-var foo = "bar" // 'var' is not preferred; should use 'let' or 'const'
-// Missing semicolon; should end with ';'
-
 function greet(name) {
-    console.log('Hello, ' + name)
-    // Missing semicolon; should end with ';'
+  console.log(`Hello, ${name}`);
+  // Missing semicolon; should end with ';'
 }
 
-greet('World')
+greet('World');
